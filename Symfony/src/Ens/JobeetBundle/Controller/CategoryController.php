3f495ed6354653eeac3fce3a2ec69fff509b3fbb
hmoreau <A362823@MA5161981.arpege.socgen>
2013-02-21 11:46:41 +0100

    Part 07 : add paginator

--- Symfony/src/Ens/JobeetBundle/Controller/CategoryController.php
+++ Symfony/src/Ens/JobeetBundle/Controller/CategoryController.php
@@ -10,7 +10,7 @@ use Ens\JobeetBundle\Entity\Category;
 */
 class CategoryController extends Controller
 {
-    public function showAction($slug)
+    public function showAction($slug,$page)
     {
         $em = $this->getDoctrine()->getEntityManager();
 
@@ -20,10 +20,23 @@ class CategoryController extends Controller
             throw $this->createNotFoundException('Unable to find Category entity.');
         }
 
-        $category->setActiveJobs($em->getRepository('EnsJobeetBundle:Job')->getActiveJobs($category->getId()));
+        $jobs_per_page = $this->container->getParameter('max_jobs_on_category');
+        
+        $activeJobsPaginator = $em->getRepository('EnsJobeetBundle:Job')->getActiveJobs($category->getId(), $jobs_per_page, ($page - 1) * $jobs_per_page);
+        $total_jobs = $activeJobsPaginator->count();
+        $last_page = ceil($total_jobs / $jobs_per_page);
+        $previous_page = $page > 1 ? $page - 1 : 1;
+        $next_page = $page < $last_page ? $page + 1 : $last_page;
+        
+        $category->setActiveJobs($activeJobsPaginator);
 
         return $this->render('EnsJobeetBundle:Category:show.html.twig', array(
             'category' => $category,
+            'last_page' => $last_page,
+            'previous_page' => $previous_page,
+            'current_page' => $page,
+            'next_page' => $next_page,
+            'total_jobs' => $total_jobs
         ));
     }
 }
